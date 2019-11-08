ca53dbf77716dcfde8c458d803ca03f67f74f871
Suelle <329496@student.mboutrecht.nl>
2019-11-08 18:29:45 +0100

    prototype

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start gebeurt lekker aan het begin! 
+    void Start()
+    {
+        
+    }
+
+    // Update is om het te updaten natuurlijk!
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        
+    
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Dit is prive, niet kijken!
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start gebeurt voor de eerste frame update!
+    void Start()
+    {
+        
+    }
+
+    // Dit update dingen in de game
+    void Update()
+    {
+         // waarom is dit zo simpel, dit heb ik al gedaan voor project zonder uitleg. 
+         horizontalInput = Input.GetAxis("Horizontal");
+          forwardInput = Input.GetAxis("Vertical");
+       
+    //okay dit zorgt er dus voor dat je naar voren gaat!
+    transform.Translate (Vector3.forward * Time.deltaTime * speed * forwardInput);
+    //en dit zorgt er dus voor dat je kan roteren enzo!
+    transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+   
+    }
+}
